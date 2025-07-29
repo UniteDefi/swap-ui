87adab7e1067a48b785e3e84713dd4b68b3c0bdb
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2025-07-29 21:00:00 +0700

    Update components/providers.tsx

--- components/providers.tsx
+++ components/providers.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type State, WagmiProvider } from "wagmi";
 import { wagmiAdapter } from "@/lib/config/wagmi";
 import { useState, type ReactNode } from "react";
+import { Toaster } from "@/components/ui/toaster";
 
 export function Providers({
   children,
@@ -18,6 +19,7 @@ export function Providers({
     <WagmiProvider config={wagmiAdapter.wagmiConfig} initialState={initialState}>
       <QueryClientProvider client={queryClient}>
         {children}
+        <Toaster />
       </QueryClientProvider>
     </WagmiProvider>
   );
