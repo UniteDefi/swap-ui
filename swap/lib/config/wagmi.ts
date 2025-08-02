354041900db0fd1b2794837ed881fd026ca8bb29
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2025-08-03 00:00:00 +0700

    Update swap/lib/config/wagmi.ts

--- swap/lib/config/wagmi.ts
+++ swap/lib/config/wagmi.ts
@@ -1,10 +1,29 @@
 import { createAppKit } from "@reown/appkit/react";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { http } from "viem";
-import { supportedChains, monadTestnet, etherlinkTestnet, seiTestnet } from "./chains";
+import { 
+  supportedChains, 
+  monadTestnet, 
+  etherlinkTestnet, 
+  seiTestnet,
+  injectiveTestnet,
+  auroraTestnet,
+  flowTestnet,
+  unichainSepolia
+} from "./chains";
+import {
+  sepolia,
+  baseSepolia,
+  arbitrumSepolia,
+  optimismSepolia,
+  polygonAmoy,
+  scrollSepolia,
+  celoAlfajores,
+  bscTestnet
+} from "viem/chains";
 
 const projectId = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID || "";
-const alchemyApiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "";
+const alchemyApiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "9MExjLYju7RbwL5KDizzG";
 
 const metadata = {
   name: "UniteDefi",
@@ -17,14 +36,21 @@ const metadata = {
 const chains = supportedChains;
 
 const transports = {
-  [11155111]: http(`https://eth-sepolia.g.alchemy.com/v2/${alchemyApiKey}`), // Sepolia
-  [80002]: http(`https://polygon-amoy.g.alchemy.com/v2/${alchemyApiKey}`), // Polygon Amoy
-  [84532]: http(`https://base-sepolia.g.alchemy.com/v2/${alchemyApiKey}`), // Base Sepolia
-  [421614]: http(`https://arb-sepolia.g.alchemy.com/v2/${alchemyApiKey}`), // Arbitrum Sepolia
-  [97]: http("https://data-seed-prebsc-1-s1.binance.org:8545"), // BSC Testnet
-  [monadTestnet.id]: http("https://testnet.monad.xyz"),
+  [sepolia.id]: http(`https://eth-sepolia.g.alchemy.com/v2/${alchemyApiKey}`),
+  [baseSepolia.id]: http(`https://base-sepolia.g.alchemy.com/v2/${alchemyApiKey}`),
+  [arbitrumSepolia.id]: http(`https://arb-sepolia.g.alchemy.com/v2/${alchemyApiKey}`),
+  [optimismSepolia.id]: http(`https://opt-sepolia.g.alchemy.com/v2/${alchemyApiKey}`),
+  [polygonAmoy.id]: http(`https://polygon-amoy.g.alchemy.com/v2/${alchemyApiKey}`),
+  [scrollSepolia.id]: http(`https://scroll-sepolia.g.alchemy.com/v2/${alchemyApiKey}`),
+  [celoAlfajores.id]: http(`https://celo-alfajores.g.alchemy.com/v2/${alchemyApiKey}`),
+  [unichainSepolia.id]: http(`https://unichain-sepolia.g.alchemy.com/v2/${alchemyApiKey}`),
+  [flowTestnet.id]: http(`https://flow-testnet.g.alchemy.com/v2/${alchemyApiKey}`),
+  [seiTestnet.id]: http(`https://sei-testnet.g.alchemy.com/v2/${alchemyApiKey}`),
+  [bscTestnet.id]: http("https://data-seed-prebsc-1-s1.binance.org:8545"),
+  [auroraTestnet.id]: http("https://testnet.aurora.dev"),
+  [injectiveTestnet.id]: http("https://testnet.sentry.chain.json-rpc.injective.network"),
   [etherlinkTestnet.id]: http("https://node.ghostnet.etherlink.com"),
-  [seiTestnet.id]: http("https://evm-rpc-arctic-1.sei-apis.com"),
+  [monadTestnet.id]: http("https://rpc.ankr.com/monad_testnet"),
 };
 
 export const wagmiAdapter = new WagmiAdapter({
