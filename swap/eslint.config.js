69bda3b1e5bc1aaccd4b8282d8d11abc266fe0ae
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2025-08-03 10:01:39 +0700

    Add swap/eslint.config.js

---
+++ swap/eslint.config.js
@@ -0,0 +1,21 @@
+const { FlatCompat } = require("@eslint/eslintrc");
+const js = require("@eslint/js");
+const path = require("path");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+module.exports = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
+      "prefer-const": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+];
